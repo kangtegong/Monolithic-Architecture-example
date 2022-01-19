5770f50755eed3f88c4a8f9e0d3a8dc4926b8efa
Kang Minchul <tegongkang@gmail.com>
2022-01-19 18:04:22 +0900

    1

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
